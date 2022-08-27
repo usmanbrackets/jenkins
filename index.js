85969479c03d31f052252f176aff14947e32c513
usman.brackets <usmanbrackets@gmail.com>
2022-08-27 16:20:26 +0500

    fea:

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
 console.log("\n\n\n\nHELLOW WORLD\n\n\n\n");
-console.log("\n\n\n\nHELLOW WORLD\n\n\n\n");
-console.log("\n\n\n\nHELLOW WORLD\n\n\n\n");
-console.log("\n\n\n\nHELLOW WORLD\n\n\n\n");
-
-console.log("\n\n\n\nHELLOW WORLD\n\n\n\n");
