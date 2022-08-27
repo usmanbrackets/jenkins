cb1decd6cde1f76c8b9afcc342cfd874826e5865
usman.brackets <usmanbrackets@gmail.com>
2022-08-27 16:33:27 +0500

    fea:123

--- index.js
+++ index.js
@@ -2,5 +2,3 @@ console.log("\n\n\n\nHELLOW WORLD\n\n\n\n");
 console.log("\n\n\n\nHELLOW WORLD\n\n\n\n");
 console.log("\n\n\n\nHELLOW WORLD\n\n\n\n");
 console.log("\n\n\n\nHELLOW WORLD\n\n\n\n");
-
-console.log("\n\n\n\nHELLOW WORLD\n\n\n\n");
