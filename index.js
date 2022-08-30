9e5d9ece604493cced2930eb3b6f23242465c02c
usman.brackets <usmanbrackets@gmail.com>
2022-08-30 23:39:16 +0500

    sf

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("\n\n\n\nHELLOW WORLD\n\n\n\n");
+const app = require("express")();
+
+app.get("/", (req, res, next) => {
+  console.log("server is running on ");
+
+  return res.status(200).json({
+    message: "server is health",
+  });
+});
+
+app.listen(8000, () => {
+  console.log("server is running on port 8000");
+});
